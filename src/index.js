a1c009daed89668fecd93c35774f3bcfdc59cd7f
mishaplx <olegorlov100oleg@gmail.com>
2021-07-13 14:26:16 +0200

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
-
 function expressionCalculator(expr) {
-    // write your solution here
+  if (checkBrackets(expr) !== 0) {
+    throw "ExpressionError: Brackets must be paired";
+  } else if (checkZiro(expr) == 0) {
+    throw "TypeError: Division by zero.";
+  } else {
+    let result = new Function("return " + expr.replace(/\s/g, ""));
+    return result();
+  }
+}
+function checkBrackets(expr) {
+  let arrExpr = expr.replace(/\s/g, "");
+  let arrExprNew = arrExpr.split("");
+  let res = 0;
+  for (let i = 0; i < arrExprNew.length; i++) {
+    const element = arrExprNew[i];
+    if (element == "(") {
+      res += 1;
+    } else if (element == ")") {
+      res -= 1;
+    }
+  }
+  return res;
+}
+function checkZiro(expr) {
+  let arrExpr = expr.replace(/\s/g, "");
+  let arrExprNew = arrExpr.split("");
+  for (let i = 0; i < arrExprNew.length; i++) {
+    if (arrExprNew[i] == "/" && arrExprNew[i + 1] == 0) {
+      return 0;
+    }
+  }
 }
 
 module.exports = {
-    expressionCalculator
-}
+  expressionCalculator,
+};
